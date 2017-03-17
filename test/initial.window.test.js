2c22abea0865d9091d273717c35d89d42ff91f1c
ermus19 <ermus19@hotmail.com>
2017-03-17 15:09:17 +0100

    Updated tests and created test.helpers

--- test/initial.window.test.js
+++ test/initial.window.test.js
@@ -1,64 +1,59 @@
 'use strict'
-var Application = require('spectron').Application
-var chai = require('chai');
-var chaiAsPromised = require('chai-as-promised');
-var assert = chai.assert;
-
-
-global.before(function () {
-  chai.should();
-  chai.use(chaiAsPromised);
-});
+var helpers = require('./test.helpers');
 
 
 describe('Application default launch:', function () {
 
-  this.timeout(10000)
+  var app = null;
 
   beforeEach(function () {
-    this.app = new Application({
-      path: `${__dirname}/../node_modules/.bin/electron`,
-      args: ['main.js']
+    return helpers.startApp().then(function (startedApp){
+      app = startedApp;
     });
-    return this.app.start();
   });
 
-  beforeEach(function () {
-    chaiAsPromised.transferPromiseness = this.app.transferPromiseness
-  });
 
   afterEach(function () {
-    if (this.app && this.app.isRunning()) {
-      return this.app.stop();
-    }
+   return helpers.stopApp(app);
   });
 
   it('Shows an initial window', function () {
-    return this.app.client.waitUntilWindowLoaded()
+    return app.client.waitUntilWindowLoaded()
       .getWindowCount().should.eventually.equal(1)
       .browserWindow.isDevToolsOpened().should.eventually.be.false
       .browserWindow.isVisible().should.eventually.be.true
       .browserWindow.isFocused().should.eventually.be.true
+      .browserWindow.isResizable().should.eventually.be.true
   });
 
-  it('Window bounds are default', function () {
-    return this.app.client.waitUntilWindowLoaded()
-      .browserWindow.getBounds().should.eventually.have.property('width').and.be.equal(1281)
-      .browserWindow.getBounds().should.eventually.have.property('height').and.be.equal(800)
+  it('Window minimun size is set', function () {
+    return app.client.waitUntilWindowLoaded()
+      .browserWindow.getMinimumSize().should.eventually.be.deep.equal([1023, 700]);
   });
+
+  it('Window default size is used', function () {
+    return app.client.waitUntilWindowLoaded()
+      .browserWindow.getBounds().should.eventually.have.property('width').and.be.equal(1023)
+      .browserWindow.getBounds().should.eventually.have.property('height').and.be.equal(700)
+  })
 
   it('Window title is Floodlight controller GUI', function () {
-    return this.app.client.waitUntilWindowLoaded()
+    return app.client.waitUntilWindowLoaded()
       .browserWindow.getTitle().should.eventually.be.equal('Floodlight controller GUI');
   });
 
+  it('Should keep bounds when resizing the window', function () {
+    return app.client.waitUntilWindowLoaded()
+      .browserWindow.set
+  })
+
   it('Should load login view', function () {
-    return this.app.client.waitUntilWindowLoaded()
+    return app.client.waitUntilWindowLoaded()
       .getUrl().should.eventually.match(/login.html/);
   });
 
   it('Should have login form invisible', function () {
-    return this.app.client.waitUntilWindowLoaded()
+    return app.client.waitUntilWindowLoaded()
       .isVisible('.form').should.eventually.be.false;
   });
 
--- test/login.view.test.js
+++ test/login.view.test.js
@@ -1,45 +1,30 @@
-'use strict'
-var Application = require('spectron').Application
-var chai = require('chai');
-var chaiAsPromised = require('chai-as-promised');
-var assert = chai.assert;
-
-
-global.before(function () {
-  chai.should();
-  chai.use(chaiAsPromised);
-});
-
+var helpers = require('./test.helpers');
 
 describe('Login window interactions', function () {
+
   this.timeout(10000);
 
-  beforeEach(function () {
-    this.app = new Application({
-      path: `${__dirname}/../node_modules/.bin/electron`,
-      args: ['main.js']
-    });
-    return this.app.start();
-  });
+  var app = null;
 
   beforeEach(function () {
-    chaiAsPromised.transferPromiseness = this.app.transferPromiseness
+    return helpers.startApp().then(function (startedApp) {
+      app = startedApp;
+    });
   });
+
 
   afterEach(function () {
-    if (this.app && this.app.isRunning()) {
-      return this.app.stop();
-    }
+    return helpers.stopApp(app);
   });
 
   it('Show IP form when clicking "Other Computer" button', function () {
-    return this.app.client.waitUntilWindowLoaded()
+    return app.client.waitUntilWindowLoaded()
       .click('#otherButton')
       .isVisible('.form').should.eventually.be.true;
   });
 
   it('Should set the IP form invisible when clicking "Back" button', function () {
-    return this.app.client.waitUntilWindowLoaded()
+    return app.client.waitUntilWindowLoaded()
       .click('#otherButton')
       .isVisible('.form').should.eventually.be.true
       .click('#backButton')
