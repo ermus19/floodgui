929ee1520be17a72c22ac8942912483129cc2a32
ermus19 <ermus19@hotmail.com>
2017-03-18 01:59:50 +0100

    last try on travis linux builds failing

--- scripts/travis-build.sh
+++ scripts/travis-build.sh
@@ -9,8 +9,7 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
 elif [[ $TRAVIS_OS_NAME == 'linux' ]]; then
 
     export DISPLAY=:99.0
-	/sbin/start-stop-daemon --start --quiet --pidfile /tmp/custom_xvfb_99.pid --make-pidfile --background --exec /usr/bin/Xvfb -- :99 -ac -screen 0 1600x1200x32
-    sleep 3
+	Xvfb :99 -screen 0 1600x1200x32 > /dev/null 2>&1 &
     ./node_modules/.bin/electron-packager .  --overwrite --platform=linux --no-prune --out=builds
 
 fi
