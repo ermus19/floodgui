8737cab6f7c41ee5951548b57e4b0bc463f3af9a
ermus19 <ermus19@hotmail.com>
2017-04-05 17:58:02 +0200

    Formatted app.routes.test

--- test/unit/app.routes.test.js
+++ test/unit/app.routes.test.js
@@ -22,7 +22,7 @@ describe('App routes:', function () {
         expect($location.path()).to.be.equal('/login');
     });
 
-    it('Expects /login controller to be login.controller', function(){
+    it('Expects /login controller to be login.controller', function () {
         $location.path('login');
         $rootScope.$apply();
         expect($route.current.controller).to.be.equal('login.controller');
@@ -34,7 +34,7 @@ describe('App routes:', function () {
         expect($location.path()).to.be.equal('/home');
     });
 
-    it('Expects /home controller to be main.controller', function(){
+    it('Expects /home controller to be main.controller', function () {
         $location.path('home');
         $rootScope.$apply();
         expect($route.current.controller).to.be.equal('main.controller');
@@ -46,7 +46,7 @@ describe('App routes:', function () {
         expect($location.path()).to.be.equal('/previous');
     });
 
-    it('Expects /previous controller to be login.controller', function(){
+    it('Expects /previous controller to be login.controller', function () {
         $location.path('previous');
         $rootScope.$apply();
         expect($route.current.controller).to.be.equal('login.controller');
@@ -58,18 +58,16 @@ describe('App routes:', function () {
         expect($location.path()).to.be.equal('/form');
     });
 
-    it('Expects /form controller to be form.controller', function(){
+    it('Expects /form controller to be form.controller', function () {
         $location.path('form');
         $rootScope.$apply();
         expect($route.current.controller).to.be.equal('form.controller');
     });
 
-    it('Expects not defined location to route /login', function(){
+    it('Expects not defined location to route /login', function () {
         $location.path('test');
         $rootScope.$apply();
         expect($location.path()).to.be.equal('/login');
         expect($route.current.controller).to.be.equal('login.controller');
-    })
-
-
+    });
 });
