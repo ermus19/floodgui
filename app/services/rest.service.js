2f49170ffde3c5cfe9b1a25d57a391e1ce98af1a
ermus19 <ermus19@hotmail.com>
2017-04-07 01:19:20 +0200

    Added stats to rest.service

--- app/services/rest.service.js
+++ app/services/rest.service.js
@@ -13,8 +13,8 @@ angular.module('rest.service', ['ngResource'])
         }
     }])
 
-    .factory('restService', ['$resource', 'configService', function ($resource, configService){
-        return{
+    .factory('restService', ['$resource', 'configService', function ($resource, configService) {
+        return {
             getMemory: function () {
                 return $resource('http://' + configService.getLocation() + ':8080/wm/core/memory/json', {}, {
                     query: {
@@ -22,6 +22,38 @@ angular.module('rest.service', ['ngResource'])
                         timeout: 5000
                     }
                 });
+            },
+            getApiHealth: function () {
+                return $resource('http://' + configService.getLocation() + ':8080/wm/core/health/json', {}, {
+                    query: {
+                        method: 'GET',
+                        timeout: 5000
+                    }
+                });
+            },
+            getVersion: function () {
+                return $resource('http://' + configService.getLocation() + ':8080/wm/core/version/json', {}, {
+                    query: {
+                        method: 'GET',
+                        timeout: 5000
+                    }
+                });
+            },
+            getUptime: function () {
+                return $resource('http://' + configService.getLocation() + ':8080/wm/core/system/uptime/json', {}, {
+                    query: {
+                        method: 'GET',
+                        timeout: 5000
+                    }
+                });
+            },
+            getFirewallStatus: function () {
+                return $resource('http://' + configService.getLocation() + ':8080/wm/firewall/module/status/json', {}, {
+                    query: {
+                        method: 'GET',
+                        timeout: 5000
+                    }
+                });
             }
-        } 
+        }
     }]);
