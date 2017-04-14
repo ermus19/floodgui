8d5fb4c743601ae3b50b07c3577b535dd13ba60f
ermus19 <ermus19@hotmail.com>
2017-04-15 01:10:13 +0200

    Devices view mac list

--- app/controllers/devices.controller.js
+++ app/controllers/devices.controller.js
@@ -2,6 +2,15 @@
 
 angular.module('devices.controller', [])
 
-    .controller('devices.controller', function ($scope, devicesService) {
+    .controller('devices.controller', function ($scope, $interval, devicesService) {
+
+        
+        var getDevices = $interval(function () {
+            
+            $scope.devices = devicesService.getDevices();
+            console.log($scope.devices);
+
+        }, 3000);
+        
 
     });
