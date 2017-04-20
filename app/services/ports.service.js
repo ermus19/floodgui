88e36838d2ec66755ce137593aed60f79b4810de
ermus19 <ermus19@hotmail.com>
2017-04-20 13:11:32 +0200

    Added port status updating to port service

--- app/services/ports.service.js
+++ app/services/ports.service.js
@@ -30,24 +30,39 @@ angular.module('ports.service', [])
                 return ports;
             }
 
+            var old_ports = ports.slice(0);
+
             ports = [];
 
             for (var i = 0; i < data[switchID].port_desc.length; i++) {
 
-                var port = { name: '-', port_number: '-', max_speed: '-', tx_speed: '-', rx_speed: '-' }
+                var port = { name: '-', port_number: '-', max_speed: '-', tx_speed: '-', rx_speed: '-', status: 'enabled' }
 
                 port.name = data[switchID].port_desc[i].name;
                 port.port_number = data[switchID].port_desc[i].port_number;
 
                 var max_speed = parseInt(data[switchID].port_desc[i].max_speed);
 
-                if(max_speed !== 0){
+                if (max_speed !== 0) {
                     port.max_speed = data[switchID].port_desc[i].max_speed + ' bit/s';
                 }
 
                 ports.push(port);
 
             }
+
+            for (var i = 0; i < old_ports.length; i++) {
+                for (var j = 0; j < ports.length; j++) {
+
+                    if (old_ports[i].port_number === ports[j].port_number) {
+
+                        ports[j].status = old_ports[i].status;
+
+                    }
+                }
+            }
+
+
             return ports;
         }
 
@@ -75,12 +90,46 @@ angular.module('ports.service', [])
             return updated_ports;
         }
 
+        function _updatePortStates(switchID, updated_ports, data) {
+
+            if (data === {} || data[switchID] === undefined) {
+                return updated_ports;
+            }
+
+            if (data[switchID].length > 0) {
+
+                for (var i = 0; i < data[switchID].length; i++) {
+                    for (var j = 0; j < updated_ports.length; j++) {
+
+                        var dataPort = parseInt(data[switchID][i].port);
+                        var currentPort = parseInt(updated_ports[j].port_number);
+
+                        if (dataPort === currentPort) {
+
+                            updated_ports[j].status = data[switchID][i].status;
+
+                        }
+
+                    }
+                }
+
+                ports = updated_ports;
+                return ports;
+
+            } else {
+
+                return updated_ports;
+
+            }
+        }
+
         return {
             getPorts: _getPorts,
             setPorts: _setPorts,
             getPortsCount: _getPortsCount,
             updatePorts: _updatePorts,
-            updatePortStats: _updatePortStats
+            updatePortStats: _updatePortStats,
+            updatePortStates: _updatePortStates,
         }
 
     }]);
