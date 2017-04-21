32264b5c6984a45dec4dc81edd9a9fea9323c597
ermus19 <ermus19@hotmail.com>
2017-04-21 19:10:15 +0200

    Updated build script with app icon

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,12 +3,12 @@
 if [[ $(uname -s) == 'Darwin' ]]; then
 
     echo "Building for Darwin"
-    ./node_modules/.bin/electron-packager . --overwrite --platform=darwin --prune=true --out=builds
+    ./node_modules/.bin/electron-packager . --overwrite --platform=darwin --icon=./app/assets/icons/app.icns --prune=true --out=builds
 
 elif [[ $(uname -s) == 'Linux' ]]; then
 
     echo "Building for Linux"
-    ./node_modules/.bin/electron-packager .  --overwrite --platform=linux --prune=true --out=builds
+    ./node_modules/.bin/electron-packager .  --overwrite --platform=linux --icon=./app/assets/icons/app.png --prune=true --out=builds
 
 else
 
